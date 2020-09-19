a517cc4faf10bdbf87b95b7444bfa45b8f92ee4c
Gavin Grant <gavingrant@gmail.com>
2020-09-19 16:39:53 -0700

    updated readme file and app.get

--- src/app.js
+++ src/app.js
@@ -1,23 +1,24 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
 
-const app = express()
+const app = express();
 
 const morganOption = (NODE_ENV === 'production')
     ? 'tiny'
     : 'common';
 
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!')
-})
+app.get('/api/*', (req, res) => {
+    res.send('Hello, world!');
+    res.json({ok: true});
+});
 
 app.use(function errorHandler(error, req, res, next) {
     let response
@@ -28,6 +29,6 @@ app.use(function errorHandler(error, req, res, next) {
         response = { message: error.message, error }
     }
     res.status(500).json(response)
-})
+});
 
-module.exports = app
+module.exports = app;
