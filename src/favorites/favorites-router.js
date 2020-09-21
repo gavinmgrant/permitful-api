62c4be1e17a29c5472c1848d300fc695d8ea3349
Gavin Grant <gavingrant@Gavins-MacBook-Pro.local>
2020-09-21 14:34:10 -0700

    added POST endpoint and tests

--- src/favorites/favorites-router.js
+++ src/favorites/favorites-router.js
@@ -8,10 +8,33 @@ const jsonParser = express.json();
 favoritesRouter
     .route('/')
     .get((req, res, next) => {
-        const knexInstance = req.app.get('db')
+        const knexInstance = req.app.get('db');
         FavoritesService.getAllFavorites(knexInstance)
             .then(favorites => {
                 res.json(favorites)
+            })
+            .catch(next)
+    })
+    .post(jsonParser, (req, res, next) => {
+        const { permit_number } = req.body;
+        const newFavorite = { permit_number }
+
+        for(const [key, value] of Object.entries(newFavorite)) {
+            if (value == null) {
+                return res.status(400).json({
+                    error: { message: `Missing '${key}' in request body` }
+                })
+            }
+        }
+        FavoritesService.insertFavorite(
+            req.app.get('db'),
+            newFavorite
+        )
+            .then(favorite => {
+                res 
+                    .status(201)
+                    .location(`/favorites/${favorite.permit_number}`)
+                    .json(favorite)
             })
             .catch(next)
     })
@@ -19,7 +42,7 @@ favoritesRouter
 favoritesRouter
     .route('/:permit_id')
     .all((req, res, next) => {
-        const knexInstance = req.app.get('db')
+        const knexInstance = req.app.get('db');
         FavoritesService.getById(knexInstance, req.params.permit_id)
             .then(favorite => {
                 if (!favorite) {
