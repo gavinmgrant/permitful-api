a36c7d569dfb9d37d44f931da7c6f992ed6e345a
Gavin Grant <gavingrant@gmail.com>
2020-09-29 15:02:39 -0700

    refactor(expiry): expanded time to 60 minutes

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql:gavingrant@localhost/permitful',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-    JWT_EXPIRY: process.env.JWT_EXPIRY || '5m',
+    JWT_EXPIRY: process.env.JWT_EXPIRY || '60m',
     CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || '*',
 };
